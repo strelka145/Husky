2dfb4529931ea1e5f654f3f9912f6f318e5b723e
iroha_nano <49903762+strelka145@users.noreply.github.com>
2023-11-02 18:31:14 +0900

    🔧fix

--- husky.js
+++ husky.js
@@ -1,5 +1,5 @@
 class Husky {
-    constructor(table,options){
+    constructor(table,options={}){
         let separatedStrings = table.split(/;/);
         separatedStrings.pop();
         if(separatedStrings.length==1){
@@ -17,13 +17,16 @@ class Husky {
         }
         this.width=125*this.temperature_list.length;
         this.svgElement =document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        if(!options.scale){
-            options.scale=1;
+        if(options.scale){
+            this.scale=options.scale;
+        }else{
+            this.scale=1;
         }
-        this.svgElement.setAttribute("height", 400*options.scale);
-        this.svgElement.setAttribute("width", this.width*options.scale);
+        this.heighest=0;
+        this.svgElement.setAttribute("height", (400-this.heighest)*this.scale);
+        this.svgElement.setAttribute("width", this.width*this.scale);
         this.svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        this.svgElement.setAttribute("viewBox", "0 0 "+this.width+" 400");
+        this.svgElement.setAttribute("viewBox", "0 "+this.heighest+" "+this.width+" "+(400-this.heighest));
     }
 
     makeGraph(targetElement){
@@ -55,6 +58,11 @@ class Husky {
             "middle"
         );
 
+        this.heighest=Math.min(...this.#calculateHeatRetentionCoordinates(this.#calculateTemperatureGraphCoordinates(this.#convertTemperatureToSVGCoordinates(this.temperature_list))).map(coord => coord[1]))-30;
+        this.svgElement.setAttribute("height", (400-this.heighest)*this.scale);
+        this.svgElement.setAttribute("width", this.width*this.scale);
+        this.svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        this.svgElement.setAttribute("viewBox", "0 "+this.heighest+" "+this.width+" "+(400-this.heighest));
         targetElement.innerHTML = '';
         targetElement.appendChild(this.svgElement);
     }
